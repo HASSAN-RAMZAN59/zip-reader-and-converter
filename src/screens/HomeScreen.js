585315d8e862b96434b6d4d06b910ddcfb66708f
HASSAN-RAMZAN59 <hasaanramzan59@gmail.com>
2026-09-12 16:55:05 +0500

    Align storage details text width with progress bar line width

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -304,7 +304,7 @@ export const HomeScreen = ({ navigation }) => {
             <View style={styles.storageCardLeft}>
               <Text style={styles.storageTitle}>Device Storage</Text>
 
-              <Text style={styles.storageText} numberOfLines={1}>
+              <Text style={styles.storageText} numberOfLines={1} adjustsFontSizeToFit>
                 <Text style={styles.storageUsed}>{storageInfo.usedVal} GB used</Text>
                 <Text style={styles.storageTotal}> / {storageInfo.totalVal} GB Total</Text>
               </Text>
@@ -475,9 +475,10 @@ const styles = StyleSheet.create({
     marginBottom: 2,
   },
   storageText: {
-    fontSize: 12,
+    fontSize: 11.5,
     fontFamily: 'Poppins-Regular',
     marginBottom: 6,
+    width: '82%',
   },
   storageUsed: {
     color: '#FFFFFF',
